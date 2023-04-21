fd026780d8e4f931342526940cf76c7868302312
Jack Watters <johnwatt@usc.edu>
2023-04-21 14:49:40 -0700

    Set up husky

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Install
 
 $ degit jackwatters45/vite-react-ts-linting
+
+## Todo
+
+- learn how to use rome
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const App = () => {
+  let a = 1;
+  
   return (
     <div>App</div>
   )
